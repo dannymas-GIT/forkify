7d268100c770cc585978ad86b754430235dd54ed
dannymas-GIT <dmas@lsit-inc.com>
2022-08-14 22:49:25 -0400

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -130,6 +130,9 @@ const controlAddRecipe = async function(newRecipe){
   }
 }
 
+const newFeature = function(){
+  console.log('Welcome to the application');
+}
 
 const init = function(){
    bookmarksView.addHandlerRender(controlBookmarks)
@@ -139,7 +142,7 @@ const init = function(){
    searchView.addHandlerSearch(controlSearchResults);
    paginationView.addHandlerClick(controlPagination);
    addRecipeView.addHandlerUpload(controlAddRecipe);
-   console.log('Welcome!!')
+   newFeature();
    
 }
 init();
